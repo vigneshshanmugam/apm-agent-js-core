b7d28fe332ff976f6d8bf8cb13f27876939030bd
Hamidreza Jahtalab <h.jahtalab@gmail.com>
2018-04-12 16:06:24 +0200

    chore: refactor Transaction and Span into class

--- src/performance-monitoring/span.js
+++ src/performance-monitoring/span.js
@@ -1,41 +1,41 @@
 var utils = require('../common/utils')
-
-function Span (signature, type, options) {
-  var opts = options || {}
-  if (typeof opts.onSpanEnd === 'function') {
-    this.onSpanEnd = opts.onSpanEnd
-  } else {
-    this.onSpanEnd = function () {}
+class Span {
+  constructor (signature, type, options) {
+    var opts = options || {}
+    if (typeof opts.onSpanEnd === 'function') {
+      this.onSpanEnd = opts.onSpanEnd
+    } else {
+      this.onSpanEnd = function () {}
+    }
+    this.signature = signature
+    this.type = type
+    this.ended = false
+    this._end = null
+    this.context = null
+    // Start timers
+    this._start = window.performance.now()
   }
-  this.signature = signature
-  this.type = type
-  this.ended = false
-  this._end = null
-  this.context = null
-  // Start timers
-  this._start = window.performance.now()
-}
+  end () {
+    this._end = window.performance.now()
 
-Span.prototype.end = function () {
-  this._end = window.performance.now()
-
-  this.ended = true
-  this.onSpanEnd(this)
-}
-
-Span.prototype.duration = function () {
-  if (utils.isUndefined(this.ended) || utils.isUndefined(this._start)) {
-    return null
+    this.ended = true
+    this.onSpanEnd(this)
   }
 
-  var diff = this._end - this._start
+  duration () {
+    if (utils.isUndefined(this.ended) || utils.isUndefined(this._start)) {
+      return null
+    }
 
-  return parseFloat(diff)
-}
+    var diff = this._end - this._start
 
-Span.prototype.setContext = function (context) {
-  if (!context) return
-  this.context = utils.merge(this.context || {}, context)
+    return parseFloat(diff)
+  }
+
+  setContext (context) {
+    if (!context) return
+    this.context = utils.merge(this.context || {}, context)
+  }
 }
 
 module.exports = Span
--- src/performance-monitoring/transaction.js
+++ src/performance-monitoring/transaction.js
@@ -2,197 +2,198 @@ var Span = require('./span')
 var utils = require('../common/utils')
 const uuidv4 = require('uuid/v4')
 
-var Transaction = function (name, type, options, logger) {
-  this.id = uuidv4()
-  this.timestamp = new Date().toISOString()
-  this.metadata = {}
-  this.name = name
-  this.type = type
-  this.ended = false
-  this._isDone = false
-  this._options = options
-  this._logger = logger
-  if (typeof options === 'undefined') {
-    this._options = {}
+class Transaction {
+  constructor (name, type, options, logger) {
+    this.id = uuidv4()
+    this.timestamp = new Date().toISOString()
+    this.metadata = {}
+    this.name = name
+    this.type = type
+    this.ended = false
+    this._isDone = false
+    this._options = options
+    this._logger = logger
+    if (typeof options === 'undefined') {
+      this._options = {}
+    }
+
+    this.contextInfo = {}
+
+    this.marks
+    if (this._options.sendVerboseDebugInfo) {
+      this.debugLog('Transaction', name, type)
+    }
+
+    this.spans = []
+    this._activeSpans = {}
+
+    this._scheduledTasks = {}
+
+    this.events = {}
+
+    this.doneCallback = function noop () {}
+
+    this._rootSpan = new Span('transaction', 'transaction', {enableStackFrames: false})
+
+    this._startStamp = new Date()
+
+    this.duration = this._rootSpan.duration.bind(this._rootSpan)
+    this.nextId = 0
+
+    this.isHardNavigation = false
   }
 
-  this.contextInfo = {}
-
-  this.marks
-  if (this._options.sendVerboseDebugInfo) {
-    this.debugLog('Transaction', name, type)
+  debugLog () {
+    if (this._options.sendVerboseDebugInfo) {
+      if (!this.contextInfo._debug) {
+        this.contextInfo._debug = {log: []}
+      }
+      if (!this.contextInfo._debug.log) {
+        this.contextInfo._debug.log = []
+      }
+      var messages = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments))
+      messages.unshift(Date.now().toString())
+      var textMessage = messages.join(' - ')
+      this.contextInfo._debug.log.push(textMessage)
+      if (this._logger) this._logger.debug(textMessage)
+    }
   }
 
-  this.spans = []
-  this._activeSpans = {}
+  addContextInfo (obj) {
+    utils.merge(this.contextInfo, obj)
+  }
 
-  this._scheduledTasks = {}
-
-  this.events = {}
-
-  this.doneCallback = function noop () {}
-
-  this._rootSpan = new Span('transaction', 'transaction', {enableStackFrames: false})
-
-  this._startStamp = new Date()
-
-  this.duration = this._rootSpan.duration.bind(this._rootSpan)
-  this.nextId = 0
-
-  this.isHardNavigation = false
-}
-
-Transaction.prototype.debugLog = function () {
-  if (this._options.sendVerboseDebugInfo) {
+  setDebugData (key, value) {
     if (!this.contextInfo._debug) {
-      this.contextInfo._debug = {log: []}
+      this.contextInfo._debug = {}
     }
-    if (!this.contextInfo._debug.log) {
-      this.contextInfo._debug.log = []
+    this.contextInfo._debug[key] = value
+  }
+
+  addNavigationTimingMarks () {
+    var marks = utils.getNavigationTimingMarks()
+    if (marks) {
+      this.addMarks({navigationTiming: marks})
     }
-    var messages = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments))
-    messages.unshift(Date.now().toString())
-    var textMessage = messages.join(' - ')
-    this.contextInfo._debug.log.push(textMessage)
-    if (this._logger) this._logger.debug(textMessage)
-  }
-}
-
-Transaction.prototype.addContextInfo = function (obj) {
-  utils.merge(this.contextInfo, obj)
-}
-
-Transaction.prototype.setDebugData = function setDebugData (key, value) {
-  if (!this.contextInfo._debug) {
-    this.contextInfo._debug = {}
-  }
-  this.contextInfo._debug[key] = value
-}
-
-Transaction.prototype.addNavigationTimingMarks = function () {
-  var marks = utils.getNavigationTimingMarks()
-  if (marks) {
-    this.addMarks({navigationTiming: marks})
-  }
-}
-
-Transaction.prototype.addMarks = function (obj) {
-  this.marks = utils.merge(this.marks || {}, obj)
-}
-
-Transaction.prototype.redefine = function (name, type, options) {
-  this.debugLog('redefine', name, type)
-  this.name = name
-  this.type = type
-  this._options = options
-}
-
-Transaction.prototype.startSpan = function (signature, type, options) {
-  // todo: should not accept more spans if the transaction is alreadyFinished
-  var transaction = this
-  this.debugLog('startSpan', signature, type)
-  var opts = typeof options === 'undefined' ? {} : options
-  opts.enableStackFrames = this._options.enableStackFrames === true && opts.enableStackFrames !== false
-
-  opts.onSpanEnd = function (trc) {
-    transaction._onSpanEnd(trc)
   }
 
-  var span = new Span(signature, type, opts)
-  span.id = this.nextId
-  this.nextId++
-  this._activeSpans[span.id] = span
-
-  return span
-}
-
-Transaction.prototype.isFinished = function () {
-  var scheduledTasks = Object.keys(this._scheduledTasks)
-  this.debugLog('isFinished scheduledTasks', scheduledTasks)
-  return (scheduledTasks.length === 0)
-}
-
-Transaction.prototype.detectFinish = function () {
-  if (this.isFinished()) this.end()
-}
-
-Transaction.prototype.end = function () {
-  if (this.ended) {
-    return
+  addMarks (obj) {
+    this.marks = utils.merge(this.marks || {}, obj)
   }
-  this.debugLog('end')
-  this.ended = true
 
-  this.addContextInfo({
-    url: {
-      location: window.location.href
+  redefine (name, type, options) {
+    this.debugLog('redefine', name, type)
+    this.name = name
+    this.type = type
+    this._options = options
+  }
+
+  startSpan (signature, type, options) {
+    // todo: should not accept more spans if the transaction is alreadyFinished
+    var transaction = this
+    this.debugLog('startSpan', signature, type)
+    var opts = typeof options === 'undefined' ? {} : options
+    opts.enableStackFrames = this._options.enableStackFrames === true && opts.enableStackFrames !== false
+
+    opts.onSpanEnd = function (trc) {
+      transaction._onSpanEnd(trc)
     }
-  })
-  this._rootSpan.end()
 
-  if (this.isFinished() === true) {
-    this._finish()
-  }
-}
+    var span = new Span(signature, type, opts)
+    span.id = this.nextId
+    this.nextId++
+    this._activeSpans[span.id] = span
 
-Transaction.prototype.addTask = function (taskId) {
-  // todo: should not accept more tasks if the transaction is alreadyFinished]
-  this.debugLog('addTask', taskId)
-  this._scheduledTasks[taskId] = taskId
-}
-
-Transaction.prototype.removeTask = function (taskId) {
-  this.debugLog('removeTask', taskId)
-  this.setDebugData('lastRemovedTask', taskId)
-  delete this._scheduledTasks[taskId]
-}
-
-Transaction.prototype.addEndedSpans = function (existingSpans) {
-  this.spans = this.spans.concat(existingSpans)
-}
-
-Transaction.prototype._onSpanEnd = function (span) {
-  this.spans.push(span)
-  span._scheduledTasks = Object.keys(this._scheduledTasks)
-  // Remove span from _activeSpans
-  delete this._activeSpans[span.id]
-}
-
-Transaction.prototype._finish = function () {
-  if (this._alreadFinished === true) {
-    return
+    return span
   }
 
-  this._alreadFinished = true
+  isFinished () {
+    var scheduledTasks = Object.keys(this._scheduledTasks)
+    this.debugLog('isFinished scheduledTasks', scheduledTasks)
+    return (scheduledTasks.length === 0)
+  }
 
-  this._adjustStartToEarliestSpan()
-  this._adjustEndToLatestSpan()
-  this.doneCallback(this)
-}
+  detectFinish () {
+    if (this.isFinished()) this.end()
+  }
 
-Transaction.prototype._adjustEndToLatestSpan = function () {
-  var latestSpan = findLatestNonXHRSpan(this.spans)
+  end () {
+    if (this.ended) {
+      return
+    }
+    this.debugLog('end')
+    this.ended = true
 
-  if (latestSpan) {
-    this._rootSpan._end = latestSpan._end
+    this.addContextInfo({
+      url: {
+        location: window.location.href
+      }
+    })
+    this._rootSpan.end()
 
-    // set all spans that now are longer than the transaction to
-    // be truncated spans
-    for (var i = 0; i < this.spans.length; i++) {
-      var span = this.spans[i]
-      if (span._end > this._rootSpan._end) {
-        span._end = this._rootSpan._end
-        span.type = span.type + '.truncated'
+    if (this.isFinished() === true) {
+      this._finish()
+    }
+  }
+
+  addTask (taskId) {
+    // todo: should not accept more tasks if the transaction is alreadyFinished]
+    this.debugLog('addTask', taskId)
+    this._scheduledTasks[taskId] = taskId
+  }
+
+  removeTask (taskId) {
+    this.debugLog('removeTask', taskId)
+    this.setDebugData('lastRemovedTask', taskId)
+    delete this._scheduledTasks[taskId]
+  }
+
+  addEndedSpans (existingSpans) {
+    this.spans = this.spans.concat(existingSpans)
+  }
+  _onSpanEnd (span) {
+    this.spans.push(span)
+    span._scheduledTasks = Object.keys(this._scheduledTasks)
+    // Remove span from _activeSpans
+    delete this._activeSpans[span.id]
+  }
+
+  _finish () {
+    if (this._alreadFinished === true) {
+      return
+    }
+
+    this._alreadFinished = true
+
+    this._adjustStartToEarliestSpan()
+    this._adjustEndToLatestSpan()
+    this.doneCallback(this)
+  }
+
+  _adjustEndToLatestSpan () {
+    var latestSpan = findLatestNonXHRSpan(this.spans)
+
+    if (latestSpan) {
+      this._rootSpan._end = latestSpan._end
+
+      // set all spans that now are longer than the transaction to
+      // be truncated spans
+      for (var i = 0; i < this.spans.length; i++) {
+        var span = this.spans[i]
+        if (span._end > this._rootSpan._end) {
+          span._end = this._rootSpan._end
+          span.type = span.type + '.truncated'
+        }
       }
     }
   }
-}
 
-Transaction.prototype._adjustStartToEarliestSpan = function () {
-  var span = getEarliestSpan(this.spans)
+  _adjustStartToEarliestSpan () {
+    var span = getEarliestSpan(this.spans)
 
-  if (span && span._start < this._rootSpan._start) {
-    this._rootSpan._start = span._start
+    if (span && span._start < this._rootSpan._start) {
+      this._rootSpan._start = span._start
+    }
   }
 }
 
