204f1c02796fd86ffd3d8888775b8877d8b4b945
Hamid <h.jahtalab@gmail.com>
2018-06-20 13:20:36 +0200

    fix: simplify patchMethod

--- src/common/patching/patch-utils.js
+++ src/common/patching/patch-utils.js
@@ -3,35 +3,52 @@ module.exports = {
   patchMethod: patchMethod
 }
 
-function apmSymbol (name) {
+function apmSymbol(name) {
   return '__apm_symbol__' + name
 }
 
-function patchMethod (target, name, patchFn) {
+function isPropertyWritable(propertyDesc) {
+  if (!propertyDesc) {
+    return true;
+  }
+
+  if (propertyDesc.writable === false) {
+    return false;
+  }
+
+  return !(typeof propertyDesc.get === 'function' && typeof propertyDesc.set === 'undefined');
+}
+
+export function attachOriginToPatched(patched, original) {
+  patched[apmSymbol('OriginalDelegate')] = original;
+}
+
+
+
+function patchMethod(target, name, patchFn) {
   var proto = target
   while (proto && !proto.hasOwnProperty(name)) {
-    proto = Object.getPrototypeOf(proto)
+    proto = Object.getPrototypeOf(proto);
   }
   if (!proto && target[name]) {
     // somehow we did not find it, but we can see it. This happens on IE for Window properties.
-    proto = target
+    proto = target;
   }
-  var delegateName = apmSymbol(name)
-  var delegate
-  if (proto && !(delegate = proto[delegateName])) {
-    delegate = proto[delegateName] = proto[name]
-    proto[name] = createNamedFn(name, patchFn(delegate, delegateName, name))
-  }
-  return delegate
-}
 
-function createNamedFn (name, delegate) {
-  try {
-    return (Function('f', 'return function ' + name + '(){return f(this, arguments)}'))(delegate) // eslint-disable-line
-  } catch (e) {
-    // if we fail, we must be CSP, just return delegate.
-    return function () {
-      return delegate(this, arguments)
+  const delegateName = apmSymbol(name);
+  var delegate;
+  if (proto && !(delegate = proto[delegateName])) {
+    delegate = proto[delegateName] = proto[name];
+    // check whether proto[name] is writable
+    // some property is readonly in safari, such as HtmlCanvasElement.prototype.toBlob
+    const desc = proto && Object.getOwnPropertyDescriptor(proto, name);
+    if (isPropertyWritable(desc)) {
+      const patchDelegate = patchFn(delegate, delegateName, name);
+      proto[name] = function () {
+        return patchDelegate(this, arguments);
+      };
+      attachOriginToPatched(proto[name], delegate);
     }
   }
+  return delegate;
 }
