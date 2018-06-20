f61be3243713a5aacf15f97141122d3a1c73ebdf
Hamid <h.jahtalab@gmail.com>
2018-06-20 13:21:01 +0200

    feat: add xhr-patch

--- src/common/patching/xhr-patch.js
+++ src/common/patching/xhr-patch.js
@@ -1,16 +1,143 @@
 var patchUtils = require('./patch-utils')
 
-var urlSympbol = patchUtils.apmSymbol('url')
-var methodSymbol = patchUtils.apmSymbol('method')
-var isAsyncSymbol = patchUtils.apmSymbol('isAsync')
+const apmSymbol = patchUtils.apmSymbol
+const patchMethod = patchUtils.patchMethod
 
-module.exports = function patchXMLHttpRequest () {
-  patchUtils.patchMethod(window.XMLHttpRequest.prototype, 'open', function (delegate) {
-    return function (self, args) {
-      self[methodSymbol] = args[0]
-      self[urlSympbol] = args[1]
-      self[isAsyncSymbol] = args[2]
-      delegate.apply(self, args)
+const XHR_TASK = apmSymbol('xhrTask');
+const XHR_LISTENER = apmSymbol('xhrListener');
+const XHR_SCHEDULED = apmSymbol('xhrScheduled');
+
+const XHR_SYNC = apmSymbol('xhrSync');
+const XHR_URL = apmSymbol('xhrURL');
+const XHR_METHOD = apmSymbol('xhrMethod');
+
+export const ADD_EVENT_LISTENER_STR = 'addEventListener';
+export const REMOVE_EVENT_LISTENER_STR = 'removeEventListener';
+
+export const ZONE_SYMBOL_ADD_EVENT_LISTENER = 'addEventListener';
+export const ZONE_SYMBOL_REMOVE_EVENT_LISTENER = 'removeEventListener';
+
+function patchXMLHttpRequest(callback) {
+
+  const XMLHttpRequestPrototype = XMLHttpRequest.prototype;
+
+  let oriAddListener = XMLHttpRequestPrototype[ZONE_SYMBOL_ADD_EVENT_LISTENER];
+  let oriRemoveListener = XMLHttpRequestPrototype[ZONE_SYMBOL_REMOVE_EVENT_LISTENER];
+  if (!oriAddListener) {
+    const XMLHttpRequestEventTarget = window['XMLHttpRequestEventTarget'];
+    if (XMLHttpRequestEventTarget) {
+      const XMLHttpRequestEventTargetPrototype = XMLHttpRequestEventTarget.prototype;
+      oriAddListener = XMLHttpRequestEventTargetPrototype[ZONE_SYMBOL_ADD_EVENT_LISTENER];
+      oriRemoveListener = XMLHttpRequestEventTargetPrototype[ZONE_SYMBOL_REMOVE_EVENT_LISTENER];
     }
-  })
+  }
+
+  const READY_STATE_CHANGE = 'readystatechange';
+  const SCHEDULE = 'schedule';
+  const INVOKE = 'invoke';
+  const CLEAR = 'clear';
+
+
+  function invokeTask(task) {
+    task.state = INVOKE
+    callback(INVOKE, task)
+  }
+
+  function scheduleTask(task) {
+    XMLHttpRequest[XHR_SCHEDULED] = false;
+    task.state = SCHEDULE
+    callback(SCHEDULE, task)
+    const data = task.data
+    const target = data.target;
+    // remove existing event listener
+    const listener = target[XHR_LISTENER];
+    if (!oriAddListener) {
+      oriAddListener = target[ZONE_SYMBOL_ADD_EVENT_LISTENER];
+      oriRemoveListener = target[ZONE_SYMBOL_REMOVE_EVENT_LISTENER];
+    }
+
+    if (listener) {
+      oriRemoveListener.call(target, READY_STATE_CHANGE, listener);
+    }
+    const newListener = target[XHR_LISTENER] = () => {
+      if (target.readyState === target.DONE) {
+        // sometimes on some browsers XMLHttpRequest will fire onreadystatechange with
+        // readyState=4 multiple times, so we need to check task state here
+        if (!data.aborted && XMLHttpRequest[XHR_SCHEDULED] && task.state === SCHEDULE) {
+          invokeTask(task)
+        }
+      }
+    };
+
+    oriAddListener.call(target, READY_STATE_CHANGE, newListener);
+
+    const storedTask = target[XHR_TASK];
+    if (!storedTask) {
+      target[XHR_TASK] = task;
+    }
+    sendNative.apply(target, data.args);
+    XMLHttpRequest[XHR_SCHEDULED] = true;
+    return task;
+  }
+
+  function clearTask(task) {
+    task.state = CLEAR
+    callback(CLEAR, task)
+    const data = task.data
+    // Note - ideally, we would call data.target.removeEventListener here, but it's too late
+    // to prevent it from firing. So instead, we store info for the event listener.
+    data.aborted = true;
+  }
+
+  const openNative =
+    patchMethod(XMLHttpRequestPrototype, 'open', () => function (self, args) {
+      self[XHR_METHOD] = args[0];
+      self[XHR_SYNC] = args[2] == false;
+      self[XHR_URL] = args[1];
+      return openNative.apply(self, args);
+    });
+
+
+  const XMLHTTPREQUEST_SOURCE = 'XMLHttpRequest.send';
+  const sendNative =
+    patchMethod(XMLHttpRequestPrototype, 'send', () => function (self, args) {
+      const task = {
+        source: XMLHTTPREQUEST_SOURCE,
+        state: '',
+        type: 'macroTask',
+        data: {
+          target: self,
+          method: self[XHR_METHOD],
+          url: self[XHR_URL],
+          args: args,
+          aborted: false
+        }
+      };
+      return scheduleTask(task);
+    });
+
+  const abortNative = patchMethod(XMLHttpRequestPrototype, 'abort', () => function (self, args) {
+    const task = self[XHR_TASK];
+    if (task && typeof task.type == 'string') {
+      // If the XHR has already completed, do nothing.
+      // If the XHR has already been aborted, do nothing.
+      // Fix #569, call abort multiple times before done will cause
+      // macroTask task count be negative number
+      if ((task.data && task.data.aborted)) {
+        return;
+      }
+      clearTask(task);
+    }
+    return abortNative.apply(self, args);
+    // Otherwise, we are trying to abort an XHR which has not yet been sent, so there is no
+    // task
+    // to cancel. Do nothing.
+  });
+}
+
+
+module.exports = {
+  patchXMLHttpRequest: patchXMLHttpRequest,
+  XHR_URL,
+  XHR_METHOD
 }
--- test/common/xhr-patch.spec.js
+++ test/common/xhr-patch.spec.js
@@ -1,17 +1,219 @@
 var xhrPatch = require('../../src/common/patching/xhr-patch')
 
 var patchUtils = require('../../src/common/patching/patch-utils')
-var urlSympbol = patchUtils.apmSymbol('url')
-var methodSymbol = patchUtils.apmSymbol('method')
+var urlSympbol = xhrPatch.XHR_URL
+var methodSymbol = xhrPatch.XHR_METHOD
 
 
 describe('xhrPatch', function () {
-  xhrPatch()
+  var events = []
 
+  xhrPatch.patchXMLHttpRequest(function (event, task) {
+    events.push({
+      event,
+      task
+    })
+  })
+
+  function mapEvent(event) {
+    delete event.task.data.target
+    event.task.data.args = [].slice.call(event.task.data.args)
+    return event
+  }
+
+  function printEvents() {
+    console.log(JSON.stringify(events.map(mapEvent), undefined, 2))
+  }
+  beforeEach(function () {
+    events = []
+  })
   it('should have correct url and method', function () {
     var req = new window.XMLHttpRequest()
     req.open('GET', '/', true)
     expect(req[urlSympbol]).toBe('/')
     expect(req[methodSymbol]).toBe('GET')
   })
+
+  it('should produce events', function (done) {
+    var req = new window.XMLHttpRequest()
+    req.open('GET', '/', true)
+    req.addEventListener("load", function () {
+      expect(events.map(mapEvent))
+        .toEqual(
+          [
+            {
+              "event": "schedule",
+              "task": {
+                "source": "XMLHttpRequest.send",
+                "state": "invoke",
+                "type": "macroTask",
+                "data": {
+                  "method": "GET",
+                  "url": "/",
+                  "args": [],
+                  "aborted": false
+                }
+              }
+            },
+            {
+              "event": "invoke",
+              "task": {
+                "source": "XMLHttpRequest.send",
+                "state": "invoke",
+                "type": "macroTask",
+                "data": {
+                  "method": "GET",
+                  "url": "/",
+                  "args": [],
+                  "aborted": false
+                }
+              }
+            }
+          ]
+        )
+      done()
+    });
+
+    req.send()
+  })
+
+  xit('should work with synchronous xhr', function (done) {
+    var req = new window.XMLHttpRequest()
+    req.open('GET', '/', false)
+    req.addEventListener("load", function () {
+      done()
+    });
+
+    req.send()
+    printEvents()
+    expect(events.map(e => e.event)).toEqual(['schedule', 'invoke'])
+  })
+
+  it('should work with failing xhr', function (done) {
+    var req = new window.XMLHttpRequest()
+    req.open('GET', '/test.json', true)
+    req.addEventListener("load", function () {
+      expect(events.map(e => e.event)).toEqual(['schedule', 'invoke'])
+      done()
+    });
+
+    req.send()
+  })
+
+  it('should work with aborted xhr', function () {
+    var req = new XMLHttpRequest()
+    req.open('GET', '/', true)
+
+    req.send()
+    req.abort()
+    expect(events.map(e => e.event)).toEqual(['schedule', 'clear'])
+  })
+
+  it('should work properly when send request multiple times on single xmlRequest instance', function (done) {
+    const req = new XMLHttpRequest();
+    req.open('get', '/', true);
+    req.send();
+    req.onload = function () {
+      req.onload = null;
+      req.open('get', '/', true);
+      req.onload = function () {
+        expect(events.map(e => e.event)).toEqual(['schedule', 'invoke', 'schedule', 'invoke'])
+        done();
+      };
+      expect(() => {
+        req.send();
+      }).not.toThrow();
+    };
+  })
+
+  it('should preserve static constants', function () {
+    expect(XMLHttpRequest.UNSENT).toEqual(0);
+    expect(XMLHttpRequest.OPENED).toEqual(1);
+    expect(XMLHttpRequest.HEADERS_RECEIVED).toEqual(2);
+    expect(XMLHttpRequest.LOADING).toEqual(3);
+    expect(XMLHttpRequest.DONE).toEqual(4);
+  });
+
+
+  it('should keep taskcount correctly when abort was called multiple times before request is done',
+    function (done) {
+      const req = new XMLHttpRequest();
+      req.open('get', '/', true);
+      req.send();
+      req.addEventListener('readystatechange', function (ev) {
+        if (req.readyState >= 2) {
+          expect(() => {
+            req.abort();
+          }).not.toThrow();
+          done();
+        }
+      });
+    });
+
+  it('should return null when access ontimeout first time without error', function () {
+    let req = new XMLHttpRequest();
+    expect(req.ontimeout).toBe(null);
+  });
+
+
+  it('should allow aborting an XMLHttpRequest after its completed', function (done) {
+    let req;
+
+    req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState === XMLHttpRequest.DONE) {
+        if (req.status !== 0) {
+          setTimeout(function () {
+            req.abort();
+            done();
+          }, 0);
+        }
+      }
+    };
+    req.open('get', '/', true);
+
+    req.send();
+  });
+
+  it('should preserve other setters', function () {
+    const req = new XMLHttpRequest();
+    req.open('get', '/', true);
+    req.send();
+    try {
+      req.responseType = 'document';
+      expect(req.responseType).toBe('document');
+    } catch (e) {
+      // Android browser: using this setter throws, this should be preserved
+      expect(e.message).toBe('INVALID_STATE_ERR: DOM Exception 11');
+    }
+  });
+
+  it('should work correctly when abort was called multiple times before request is done',
+    function (done) {
+
+      const req = new XMLHttpRequest();
+      req.open('get', '/', true);
+      req.send();
+      req.addEventListener('readystatechange', function (ev) {
+        if (req.readyState >= 2) {
+          expect(() => {
+            req.abort();
+          }).not.toThrow();
+          done();
+        }
+      });
+    });
+
+
+  it('should not throw error when get XMLHttpRequest.prototype.onreadystatechange the first time',
+    function () {
+      const func = function () {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange;
+      };
+      expect(func).not.toThrow();
+    });
+
 })
+
+
