31b624dd49c646d1abcb6a96b81324d02f0bb8d3
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-01-04 14:19:19 +0100

    chore: refactor config service and subscription

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-    "env": {
-        "es6": true
-    },
-    "rules": {
-        "max-len": ["error", { "code": 100 }],
-        "no-unused-vars": "error"
-    }
+  env: {
+    es6: true
+  },
+  rules: {
+    'max-len': ['error', { code: 100 }],
+    'no-unused-vars': 'error',
+    'space-before-function-paren': 'error'
+  }
 }
