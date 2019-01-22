7e6775f871e0632f05516f616142ae401067eeca
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-01-22 16:15:29 +0100

    chore: add license header on all files (#31)
    * chore: add license header on all files
    
    * fix formatting

--- src/opentracing/span.js
+++ src/opentracing/span.js
@@ -1,3 +1,28 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017-present, Elasticsearch BV
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 const { Span: otSpan } = require('opentracing/lib/span')
 const { extend, getTimeOrigin } = require('../common/utils')
 const Transaction = require('../performance-monitoring/transaction')
@@ -54,8 +79,8 @@ class Span extends otSpan {
 
     this.span.addTags(tags)
   }
-
-  _log (log, timestamp) {
+  // eslint-disable-next-line
+  _log(log, timestamp) {
     if (log.event === 'error') {
       if (log['error.object']) {
         this.__tracer.errorLogging.logError(log['error.object'])
